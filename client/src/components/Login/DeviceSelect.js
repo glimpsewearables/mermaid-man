df3047c3bc449c42a0fe959049603c375b81b8eb
KeeegJordan <keeganbbjordan@gmail.com>
2018-12-01 15:01:21 -0800

    removed typo

--- client/src/components/Login/DeviceSelect.js
+++ client/src/components/Login/DeviceSelect.js
@@ -8,7 +8,7 @@ function isValidUser(user, currUsers = [], devices = []){
   // debugging
   console.log(currUsers, devices)
 
-  if( findDeviceId(user.device, devices).length < 1 ){git
+  if( findDeviceId(user.device, devices).length < 1 ){
     return "Invalid deviceID, please contact Glimpse member for assistance."
   }if( isNameTaken(user.name, currUsers).length >= 1 ){
     return "Name already used,"
